8b9281e36f844356fdc624fdda15503b5eeda3e9
Andrea Barberio <insomniac@slackware.it>
2019-09-18 13:19:37 +0100

    Added license headers
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

--- handler/handler.go
+++ handler/handler.go
@@ -1,3 +1,7 @@
+// Copyright 2018-present the CoreDHCP Authors. All rights reserved
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 package handler
 
 import (
--- plugins/example/plugin.go
+++ plugins/example/plugin.go
@@ -1,3 +1,7 @@
+// Copyright 2018-present the CoreDHCP Authors. All rights reserved
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 package example
 
 // This is an example plugin that inspects a packet and prints it out. The code
