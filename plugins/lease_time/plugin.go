1a589e9a0fa3f0d1b0132029fbac7eaa9dbb07e9
kevin <kworm@missouri-telecom.com>
2019-11-19 10:33:55 -0600

    Remove DHCPv6 references since a default lease time is only a DHCPv4 concept.
    Signed-off-by: kevin <kworm@missouri-telecom.com>

--- plugins/lease_time/plugin.go
+++ plugins/lease_time/plugin.go
@@ -13,25 +13,16 @@ import (
 	"github.com/coredhcp/coredhcp/logger"
 	"github.com/coredhcp/coredhcp/plugins"
 	"github.com/insomniacslk/dhcp/dhcpv4"
-	"github.com/insomniacslk/dhcp/dhcpv6"
 )
 
-var log = logger.GetLogger("plugins/lease_time")
-
 func init() {
-	plugins.RegisterPlugin("lease_time", setupLeaseTime6, setupLeaseTime4)
+	plugins.RegisterPlugin("lease_time", nil, setupLeaseTime4)
 }
 
 var (
-	v6LeaseTime time.Duration
+	log = logger.GetLogger("plugins/lease_time")
 	v4LeaseTime time.Duration
 )
-
-// Handler6 handles DHCPv6 packets for the lease_time plugin.
-func Handler6(req, resp dhcpv6.DHCPv6) (dhcpv6.DHCPv6, bool) {
-	log.Warning("not currently supported for IPv6")
-	return resp, false
-}
 
 // Handler4 handles DHCPv4 packets for the lease_time plugin.
 func Handler4(req, resp *dhcpv4.DHCPv4) (*dhcpv4.DHCPv4, bool) {
@@ -54,17 +45,4 @@ func setupLeaseTime4(args ...string) (handler.Handler4, error) {
 	}
 	v4LeaseTime = leaseTime
 	return Handler4, nil
-}
-
-func setupLeaseTime6(args ...string) (handler.Handler6, error) {
-	log.Print("loading `lease_time` plugin for DHCPv6")
-	if len(args) < 1 {
-		return nil, errors.New("no default lease time provided")
-	}
-	leaseTime, err := time.ParseDuration(args[0])
-	if err != nil {
-		return nil, fmt.Errorf("invalid duration: %v", args[0])
-	}
-	v6LeaseTime = leaseTime
-	return Handler6, nil
 }
