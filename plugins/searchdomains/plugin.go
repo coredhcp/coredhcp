67aa4748c535cb4308194b78e6d93abd855fe549
William Rory Kronmiller <wkronmiller@Williams-MacBook-Pro.local>
2020-12-26 08:03:08 -0500

    Address Review Comments
    1. Return a copy of the original domain search list from the plugin,
       ensuring that down-stream plugins cannot corrupt the original search
    list config.
    2. Update the logger calls to be more explicit about what is being
       logged.
    
    Signed-off-by: William Rory Kronmiller <wkronmiller@Williams-MacBook-Pro.local>

--- plugins/searchdomains/plugin.go
+++ plugins/searchdomains/plugin.go
@@ -34,35 +34,44 @@ var Plugin = plugins.Plugin{
 	Setup4: setup4,
 }
 
-// These are the DHCP options that are added by the plugin.
+// These are the DNS search domains that are set by the plugin.
 // Note that DHCPv4 and DHCPv6 options are totally independent.
 // If you need the same settings for both, you'll need to configure
 // this plugin once for the v4 and once for the v6 server.
-var v4Option dhcpv4.Option
-var v6Option dhcpv6.Option
+var v4SearchList []string
+var v6SearchList []string
+
+// copySlice creates a new copy of a string slice in memory.
+// This helps to ensure that downstream plugins can't corrupt
+// this plugin's configuration
+func copySlice(original []string) []string {
+	copied := make([]string, len(original))
+	copy(copied, original)
+	return copied
+}
 
 func setup6(args ...string) (handler.Handler6, error) {
-	v6Option = dhcpv6.OptDomainSearchList(&rfc1035label.Labels{
-		Labels: args,
-	})
-	log.Println(v6Option)
+	v6SearchList = args
+	log.Printf("Registered domain search list (DHCPv6) %s", v6SearchList)
 	return domainSearchListHandler6, nil
 }
 
 func setup4(args ...string) (handler.Handler4, error) {
-	v4Option = dhcpv4.OptDomainSearch(&rfc1035label.Labels{
-		Labels: args,
-	})
-	log.Println(v4Option)
+	v4SearchList = args
+	log.Printf("Registered domain search list (DHCPv4) %s", v4SearchList)
 	return domainSearchListHandler4, nil
 }
 
 func domainSearchListHandler6(req, resp dhcpv6.DHCPv6) (dhcpv6.DHCPv6, bool) {
-	resp.UpdateOption(v6Option)
+	resp.UpdateOption(dhcpv6.OptDomainSearchList(&rfc1035label.Labels{
+		Labels: copySlice(v6SearchList),
+	}))
 	return resp, false
 }
 
 func domainSearchListHandler4(req, resp *dhcpv4.DHCPv4) (*dhcpv4.DHCPv4, bool) {
-	resp.UpdateOption(v4Option)
+	resp.UpdateOption(dhcpv4.OptDomainSearch(&rfc1035label.Labels{
+		Labels: copySlice(v4SearchList),
+	}))
 	return resp, false
 }
