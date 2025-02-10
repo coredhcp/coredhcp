6e89a32c95db2c4d2974d5850b8f263094e81d6f
Konstantin Zamyakin <dev@weblab.pro>
2025-02-11 00:43:28 +0300

    update copyright notice

--- plugins/consul_range/plugin.go
+++ plugins/consul_range/plugin.go
@@ -1,7 +1,3 @@
-// Copyright 2018-present the CoreDHCP Authors. All rights reserved
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
-
 package consulrangeplugin
 
 import (
